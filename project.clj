7a472006dc965238e9b7fa9ec725026107cc6f9c
Hugo Duncan <hugo@hugoduncan.org>
2013-04-20 13:25:53 -0400

    Fix pallet version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/palletops/graphite-crate"
   :license {:name "All rights reserved"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [com.palletops/pallet "0.8.0-SNAPSHOT"]]
+                 [com.palletops/pallet "0.8.0-beta.7"]]
   :resource {:resource-paths ["doc-src"]
              :target-path "target/classes/pallet_crate/collectd_crate/"
              :includes [#"doc-src/USAGE.*"]}
