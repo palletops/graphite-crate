2ea73dc56d79088a210cf2568d6b2d6cf1b9fbf2
Antoni Batchelli <tbatchelli@acm.org>
2014-04-18 15:16:29 -0700

    remove doc creation tasks

--- project.clj
+++ project.clj
@@ -5,8 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:url "git@github.com:palletops/graphite-crate.git"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [com.palletops/pallet "0.8.0-beta.9"]]
-  :resource {:resource-paths ["doc-src"]
-             :target-path "target/classes/pallet_crate/collectd_crate/"
-             :includes [#"doc-src/USAGE.*"]}
-  :prep-tasks ["resource" "crate-doc"])
+                 [com.palletops/pallet "0.8.0-beta.9"]])
